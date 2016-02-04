62e75cf14c9efea307247913b595b58809e2a5e3
Arshad Chummun <arshad@chummun.com>
2016-02-04 17:12:58 +0400

    Update bartlett and brenham-flipped layouts

--- radix_bartlett/layout--radix-bartlett.tpl.php
+++ radix_bartlett/layout--radix-bartlett.tpl.php
@@ -4,69 +4,81 @@
  * Template for Radix Bartlett.
  */
 ?>
-<div class="radix-bartlett <?php print implode(' ', $classes); ?>"<?php print backdrop_attributes($attributes); ?>>
+<div class="<?php print implode(' ', $classes); ?> layout--bartlett"<?php print backdrop_attributes($attributes); ?>>
   <?php if (!empty($content['header'])): ?>
-    <header id="header" class="header" role="header">
-      <div class="container">
-        <?php print $content['header']; ?>
-      </div>
+    <header class="layout__header" role="header">
+      <?php print $content['header']; ?>
     </header>
   <?php endif; ?>
 
   <?php if ($messages): ?>
-    <section class="messages container">
-      <?php print $messages; ?>
+    <section class="layout__messages">
+      <div class="container">
+        <?php print $messages; ?>
+      </div>
     </section>
   <?php endif; ?>
 
-  <main class="main container" role="main">
-    <div class="page-header">
-      <a id="main-content"></a>
+  <div class="layout__title">
+    <div class="container">
       <?php print render($title_prefix); ?>
       <?php if ($title): ?>
-        <h1 class="title" id="page-title">
-          <?php print $title; ?>
-        </h1>
+        <div class="page-header">
+          <h1><?php print $title; ?></h1>
+        </div>
       <?php endif; ?>
       <?php print render($title_suffix); ?>
     </div>
+  </div>
 
-    <?php if ($tabs): ?>
-      <div class="tabs">
+  <?php if ($tabs): ?>
+    <div class="layout__tabs">
+      <div class="container">
         <?php print $tabs; ?>
       </div>
-    <?php endif; ?>
+    </div>
+  <?php endif; ?>
 
-    <?php print $action_links; ?>
-    <div class="container-fluid">
+  <?php if ($action_links): ?>
+    <div class="layout__action-links">
+      <div class="container">
+        <?php print $action_links; ?>
+      </div>
+    </div>
+  <?php endif; ?>
+
+  <main class="layout__main" role="main">
+
+    <div class="container">
       <div class="row">
         <!-- Sidebar -->
-        <aside class="col-md-4 radix-layouts-sidebar">
+        <aside class="layout__sidebar col-md-4">
           <?php print $content['sidebar']; ?>
         </aside>
 
         <div class="col-md-8">
           <div class="row">
-            <div class="col-md-12 radix-layouts-contentheader">
+            <div class="layout__contentheader col-md-12 ">
               <?php print $content['contentheader']; ?>
             </div>
-            <div class="col-md-6 radix-layouts-contentcolumn1">
+            <div class="layout__contentcolumn1 col-md-6">
               <?php print $content['contentcolumn1']; ?>
             </div>
-            <div class="col-md-6 radix-layouts-contentcolumn2">
+            <div class="layout__contentcolumn2 col-md-6">
               <?php print $content['contentcolumn2']; ?>
             </div>
           </div>
         </div>
+        
       </div>
     </div>
   </main>
 
   <?php if ($content['footer']): ?>
-    <footer id="footer" class="footer" role="footer">
+    <footer class="layout__footer" role="footer">
       <div class="container">
         <?php print $content['footer']; ?>
       </div>
     </footer>
   <?php endif; ?>
-</div><!-- /.radix-bartlett -->
+</div>
--- radix_brenham_flipped/layout--radix-brenham-flipped.tpl.php
+++ radix_brenham_flipped/layout--radix-brenham-flipped.tpl.php
@@ -4,52 +4,65 @@
  * Template for Radix Brenham Flipped.
  */
 ?>
-<div class="radix-brenham-flipped <?php print implode(' ', $classes); ?>"<?php print backdrop_attributes($attributes); ?>>
+<div class="<?php print implode(' ', $classes); ?> layout--brenham-flipped"<?php print backdrop_attributes($attributes); ?>>
   <?php if (!empty($content['header'])): ?>
-    <header id="header" class="header" role="header">
-      <div class="container">
-        <?php print $content['header']; ?>
-      </div>
+    <header class="layout__header" role="header">
+      <?php print $content['header']; ?>
     </header>
   <?php endif; ?>
 
+  <?php if (!empty($content['top'])): ?>
+    <div class="layout__top">
+      <div class="container">
+        <?php print $content['top']; ?>
+      </div>
+    </div>
+  <?php endif; ?>
+
   <?php if ($messages): ?>
-    <section class="messages container">
-      <?php print $messages; ?>
+    <section class="layout__messages">
+      <div class="container">
+        <?php print $messages; ?>
+      </div>
     </section>
   <?php endif; ?>
 
-  <main class="main container" role="main">
-    <div class="page-header">
-      <a id="main-content"></a>
+  <div class="layout__title">
+    <div class="container">
       <?php print render($title_prefix); ?>
       <?php if ($title): ?>
-        <h1 class="title" id="page-title">
-          <?php print $title; ?>
-        </h1>
+        <div class="page-header">
+          <h1><?php print $title; ?></h1>
+        </div>
       <?php endif; ?>
       <?php print render($title_suffix); ?>
     </div>
+  </div>
 
-    <?php if ($tabs): ?>
-      <div class="tabs">
+  <?php if ($tabs): ?>
+    <div class="layout__tabs">
+      <div class="container">
         <?php print $tabs; ?>
       </div>
-    <?php endif; ?>
+    </div>
+  <?php endif; ?>
 
-    <?php print $action_links; ?>
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-md-12 radix-layouts-top panel-panel">
-          <?php print $content['top']; ?>
-        </div>
+  <?php if ($action_links): ?>
+    <div class="layout__action-links">
+      <div class="container">
+        <?php print $action_links; ?>
       </div>
+    </div>
+  <?php endif; ?>
 
+  <main class="layout__main" role="main">
+
+    <div class="container">
       <div class="row">
-        <div class="col-md-9 radix-layouts-content panel-panel">
+        <div class="layout__content col-md-9">
           <?php print $content['content']; ?>
         </div>
-        <div class="col-md-3 radix-layouts-sidebar panel-panel">
+        <div class="layout__sidebar col-md-3">
           <?php print $content['sidebar']; ?>
         </div>
       </div>
@@ -57,10 +70,10 @@
   </main>
 
   <?php if ($content['footer']): ?>
-    <footer id="footer" class="footer" role="footer">
+    <footer class="layout__footer" role="footer">
       <div class="container">
         <?php print $content['footer']; ?>
       </div>
     </footer>
   <?php endif; ?>
-</div><!-- /.radix-brenham-flipped -->
+</div>
